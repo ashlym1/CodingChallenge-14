811609bd6eb644faca918277f8dd6973f7fdf5f0
ashlym1 <ashlym3@usf.edu>
2025-03-09 16:00:42 -0400

    Task 2 - Support Tickets Dynamic Addition

---
+++ cc_14.js
@@ -0,0 +1,35 @@
+// Task 2: Adding support Tickets Dynamically :
+// selecting the container for support tickets 
+const ticketContainer = document.getElementById("ticketContainer");
+function addTicker(CustomerName, IssueDescription,priorityLevel) {
+    const ticket= document.createElement ("div"); // Creating the ticker card
+    ticket.className="ticket-card"; // This assigns the class for syling 
+
+    const nameHeading = document.createElement("h3"); // Heading for customer name
+    nameHeading.textContent = customerName;  
+
+    const issuePara = document.createElement("p"); // issue description
+    issuePara.textContent = issueDescription;
+
+    const priorityLabel = document.createElement("p"); // the priority label
+    priorityLabel.textContent = `Priority: ${priorityLevel}`;
+
+    const resolveButton = document.createElement("button"); // resolve button
+    resolveButton.textContent = "Resolve"; 
+    resolveButton.className = "resolve-button"; // Assigning the  class for styling
+   
+    // resolve button functionaloty 
+resolveButton.addEventListener("click",function() {
+    ticketContainer.removeChild(ticket);// removing the ticket when it's clicked
+});
+   //adding the elements to the ticket
+   ticket.appendChild(nameHeading);
+   ticket.appendChild(issuePara);
+   ticket.appendChild(priorityLabel);
+   ticket.appendChild(resolveButton);
+   // appendinf the tickets toi the ticket container 
+   ticketContainer.appendChild(ticket);
+}
+// ** Test case 
+addTicket("Miriam Jennifer"," Warning: Website is not loading", "High");
+addTicket("Tito Blue", "Warning: Paymentnot accepted", "Medium")
